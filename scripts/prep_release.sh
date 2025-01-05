82ee4569fe81a44beeb715ea4a60e631e91e7ac9
Pete Cornish <outofcoffee@gmail.com>
2025-01-05 20:10:11 +0000

    build: simplifies release script.

--- scripts/prep_release.sh
+++ scripts/prep_release.sh
@@ -11,17 +11,3 @@ fi
 cd "$( git rev-parse --show-toplevel )"
 
 npm version "$RELEASE_TYPE"
-
-# update lockfile etc.
-npm install
-
-NEW_VERSION="$( npm pkg get version | sed 's/"//g' )"
-echo -e "New version: ${NEW_VERSION}\nCommit changes and create tag (y/N)?"
-read -r CONFIRM_COMMIT
-if [[ -z "$CONFIRM_COMMIT" || "y" != "$CONFIRM_COMMIT" ]]; then
-  echo "Aborted"
-  exit 1
-fi
-
-git commit -a -m"build: release ${NEW_VERSION}."
-git tag "$NEW_VERSION"
