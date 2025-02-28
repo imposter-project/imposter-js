17f4f9fa2bff816a4602d35b0b6eeb34120e9cfa
nickhealDD <98758345+nickhealDD@users.noreply.github.com>
2025-02-28 17:47:12 +0000

    feat: dual build cjs and esm

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  external: ["child_process", "path", "fs", "http", "os", "net"],
+  input: "src/index.ts",
+  output: [
+    {
+      exports: "named",
+      file: "dist/cjs/index.cjs",
+      format: "cjs",
+    },
+    {
+      exports: "named",
+      file: "dist/esm/index.js",
+      format: "es",
+    },
+  ],
+  plugins: [typescript()],
+};
