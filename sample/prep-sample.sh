e60929dc3ca0a3541c4b0b890272ee5af970889b
Pete Cornish <outofcoffee@gmail.com>
2025-03-05 11:20:30 +0000

    build: use relative paths for main/module/types package properties.

--- sample/prep-sample.sh
+++ sample/prep-sample.sh
@@ -26,13 +26,7 @@ function set_dep_version() {
   echo "${MODIFIED_PKG_JSON}" | jq > ./package.json
 }
 
-function set_main_path() {
-  MODIFIED_PKG_JSON=$(jq '.main = "index.js"' "${SAMPLE_DIR}/package.json")
-  echo "${MODIFIED_PKG_JSON}" | jq > "${SAMPLE_DIR}/package.json"
-}
-
 cd "${SCRIPT_DIR}"
 
 package_dep
 set_dep_version
-set_main_path
