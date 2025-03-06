6a2ac49c49a634c720d0f76c8011a2fd39dbd00e
Pete Cornish <outofcoffee@gmail.com>
2025-03-06 12:36:46 +0000

    test: simplifies dependency prep script.

--- examples/example-javascript/prep-example.sh
+++ examples/example-javascript/prep-example.sh
@@ -2,30 +2,21 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SAMPLE_DIR="${SCRIPT_DIR}/tmp/imposter"
-NEW_MODULE_PATH="file:./tmp/imposter"
+NEW_MODULE_PATH="file:../../dist"
 
-function package_dep() {
-  if [[ -d ./tmp ]]; then
-    rm -rf ./tmp
-  fi
-
-  mkdir -p ./tmp
-
-  pushd ../
-  npm install
+function build_dep() {
+  pushd ../../
+  npm ci
   npm run build
   popd || exit 1
-
-  cp -r ../../dist "${SAMPLE_DIR}"
 }
 
-function set_dep_version() {
+function set_dep_path() {
   MODIFIED_PKG_JSON=$(jq --arg moduleVer "${NEW_MODULE_PATH}" '.devDependencies["@imposter-js/imposter"] = ($moduleVer)' ./package.json)
   echo "${MODIFIED_PKG_JSON}" | jq > ./package.json
 }
 
 cd "${SCRIPT_DIR}"
 
-package_dep
-set_dep_version
+build_dep
+set_dep_path
